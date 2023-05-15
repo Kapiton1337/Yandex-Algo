1bc24cadff6b515495042d568924e50e7f4fb567
Kapitosha <artpolevtsov63@mail.ru>
2023-05-15 16:16:29 +0300

    Petya

---
+++ Petta/Petta/Petta.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string x, z, y, tmp;
+    cin >> x >> z;
+
+    char* ptr_x = &x[x.length() - 1];
+    char* ptr_z = &z[z.length() - 1];
+
+    while (ptr_z != &z[0] - 1)
+    {
+        //tmp.push_back(*ptr_z);
+        tmp.insert(tmp.begin(), *ptr_z);
+        if (*ptr_z != *ptr_x) { 
+            y = tmp;
+            ptr_z--;
+            ptr_x = &x[x.length() - 1];
+        }
+        else if (ptr_x == &x[0]) {
+            ptr_x = &x[x.length() - 1];
+            ptr_z--;
+        }
+        else
+        {
+            ptr_z--;
+            ptr_x--;
+        }
+    }
+
+    cout << y;
+}
---
+++ conduse/conduse/conduse.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+long long confuse(int N, int K, long long* A) {
+    long long max = A[0];
+    long long min = A[0];
+    long long sum = 0;
+    for (long long i = 1; i < N; i++)
+    {
+        min = min > A[i] ? A[i] : min;
+        max = max < A[i] ? A[i] : max;
+    }
+    return max - min;
+}
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    long long* A = new long long[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> A[i];
+    }
+    cout << confuse(N, K, A);
+}
